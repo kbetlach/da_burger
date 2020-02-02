c68c95497fb9e6ca77d93b01bd9b18a78cf8c351
kbetlach <kbetlach@gmail.com>
2020-02-02 14:57:50 -0600

    updated service workers and manifest files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,58 +1,58 @@
-//const FILES_TO_CACHE = [
-//    "/index.html",
-//    "/favorites.html",
-//    "/topic.html",
-//    "/assets/css/style.css",
-//    "/dist/app.bundle.js",
-//    "/dist/favorites.bundle.js",
-//    "/dist/topic.bundle.js"
-//  ];
-//  
-//      ALL FILES UP HERE? ^^
-//  
-//  const PRECACHE = "precache-v1";
-//  const RUNTIME = "runtime";
-//
-//      BOILERPLATE?
-//  
-//  self.addEventListener("install", event => {
-//    event.waitUntil(
-//      caches.open(PRECACHE)
-//        .then(cache => cache.addAll(FILES_TO_CACHE))
-//        .then(self.skipWaiting())
-//    );
-//  });
-//  
-//  // The activate handler takes care of cleaning up old caches.
-//  self.addEventListener("activate", event => {
-//    const currentCaches = [PRECACHE, RUNTIME];
-//    event.waitUntil(
-//      caches.keys().then(cacheNames => {
-//        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-//      }).then(cachesToDelete => {
-//        return Promise.all(cachesToDelete.map(cacheToDelete => {
-//          return caches.delete(cacheToDelete);
-//        }));
-//      }).then(() => self.clients.claim())
-//    );
-//  });
-//  
-//  self.addEventListener("fetch", event => {
-//    if (event.request.url.startsWith(self.location.origin)) {
-//      event.respondWith(
-//        caches.match(event.request).then(cachedResponse => {
-//          if (cachedResponse) {
-//            return cachedResponse;
-//          }
-//  
-//          return caches.open(RUNTIME).then(cache => {
-//            return fetch(event.request).then(response => {
-//              return cache.put(event.request, response.clone()).then(() => {
-//                return response;
-//              });
-//            });
-//          });
-//        })
-//      );
-//    }
-//  });
+const FILES_TO_CACHE = [
+    "/manifest.webmanifest",
+    "/assets/css/burger_style.css",
+    "/assets/img/burger.png",
+    "/assets/js/burger.js",
+    "../config/connection.js",
+    "../config/orm.js",
+    "../controllers/burger_controller.js",
+    "../db/burger_seeds.sql",
+    "../db/burger_schema.sql",
+    "../models/burger.js",
+    "../server.js"
+  ];
+
+  const PRECACHE = "precache-v1";
+  const RUNTIME = "runtime";
+  
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(PRECACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(RUNTIME).then(cache => {
+            return fetch(event.request).then(response => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+  });
