204aa83395cdbc7d62beb4386fe936d5b62b2faf
kbetlach <kbetlach@gmail.com>
2020-02-04 15:47:40 -0600

    adding an indexedDB and linked to main handlebars. js and service worker tweaks

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,69 +1,74 @@
 $(function() {
-    $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevoured = $(this).data("newdevoured");
-  
-      var newDevouredState = {
-        devoured: newDevoured
-      };
-  
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
 
-      if (newDevouredState.devoured) {
-  
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed eat to", newDevoured);
-      
-          location.reload();
-        }
-      );
-    }
+    var newDevouredState = {
+      devoured: newDevoured
+    };
 
-    else {
-      $.ajax("/api/burgersTwo/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed eat to", newDevoured);
-      
-          location.reload();
-        }
-      );
-    }
+
+    if (newDevouredState.devoured) {
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed eat to", newDevoured);
+    
+        location.reload();
+      }
+    );
+  }
+
+  else {
+    $.ajax("/api/burgersTwo/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed eat to", newDevoured);
+    
+        location.reload();
+      }
+    );
+  }
+});
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+    };
+
+    $.ajax("/api/burgers/", {
+      type: "POST",
+      data: newBurger,
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        console.log("error01");
+        console.log("Failed to Save DB - Saving Locally");
+        saveRecord(newBurger);
+    }})
+      .then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
   });
-  
-    $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#ca").val().trim(),
-      };
-  
-      $.ajax("/api/burgers/", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      $.ajax("/api/burgers/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted burger", id);
-          location.reload();
-        }
-      );
-    });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        location.reload();
+      }
+    );
   });
+});
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,55 +1,48 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/manifest.webmanifest",
-    "/assets/css/burger_style.css",
-    "/assets/img/burger.png",
-    "/assets/js/burger.js",
-    "../config/connection.js",
-    "../config/orm.js",
-    "../controllers/burger_controller.js",
-    "../db/burger_seeds.sql",
-    "../db/burger_schema.sql",
-    "../models/burger.js",
-    "../server.js"
-  ];
+  "/",
+  "/assets/css/burger_style.css",
+  "/assets/img/burger.png",
+  "/assets/js/burgers.js",
+  "https://code.jquery.com/jquery.js"
+];
 
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1"
-  
-  //installation
-  self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // activation
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  // fetching
-  self.addEventListener("fetch", function(evt) {
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
-      })
-    );
-  });
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1"
+
+//installation
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// activation
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetching
+self.addEventListener("fetch", function(evt) {
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
